f28b640879dbd636ea41c4c4019fdf8f8d129fdf
selawik <121557430+selawik@users.noreply.github.com>
2023-08-19 18:10:31 +0300

    Add yandex map ios configuration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,14 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let yandexMapKitApiKey = Bundle.main.object(forInfoDictionaryKey: "YandexMapApiKey") as? String
+
+    if (yandexMapKitApiKey != nil) {
+      YMKMapKit.setApiKey(yandexMapKitApiKey!)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
