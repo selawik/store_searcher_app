812a5fd18f267497e777cf0c8863aa27e6a173e3
selawik <121557430+selawik@users.noreply.github.com>
2023-08-19 18:31:59 +0300

    Add android yandex map setup

--- android/app/src/main/kotlin/com/example/yandex_map_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yandex_map_test/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.yandex_map_test
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        var yandexMapKitApiKey = BuildConfig.YANDEX_MAPKIT_API_KEY
+
+        MapKitFactory.setApiKey(yandexMapKitApiKey)
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
